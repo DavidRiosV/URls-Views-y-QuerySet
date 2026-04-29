dcdb0a5f219a7d8fa04b4ee0028544c00c3e7429
David Rios <david.rios.valencia.al@iespoligonosur.org>
2026-04-29 19:41:17 +0200

    Menu1

--- Tienda/views.py
+++ Tienda/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from Tienda.models import Usuario,Perfil_Usuario,Marca,Descuento,Prenda,Inventario,Pedido,Opinion,Inventario,Cesta
+from django.db.models import Q,Avg,Max,Min,Prefetch
 
-# Create your views here.
+# Menu de inicio
+def index(request):
+    return render(request, 'index.html')
--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/david/Recuperacion Jorge/Inicio-y-Modelos/myvenv'
+set -gx VIRTUAL_ENV '/home/david/Recuperacion Jorge/URls-Views-y-QuerySet/myvenv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
